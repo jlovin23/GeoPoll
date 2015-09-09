43d12bdbdc8ae7929a282500306d34638688443f
jlovin23 <josephlovinger@Josephs-MacBook-Pro-2.local>
2015-09-09 11:35:48 -0400

    got dropdown working

--- GeoPoll/FeedViewController.swift
+++ GeoPoll/FeedViewController.swift
@@ -13,9 +13,14 @@ class FeedViewController: UIViewController {
     
     @IBOutlet weak var dropdownView: UIView!
     @IBOutlet weak var dropdownActionButton: UIButton!
+    
+    var popupDrawerIsShowing = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
         self.dropdownView.frame = CGRectMake(0, self.view.frame.size.height-dropdownActionButton.frame.size.height, dropdownView.frame.size.width, dropdownView.frame.size.height)
     }
     
@@ -38,6 +43,19 @@ class FeedViewController: UIViewController {
     @IBAction func addFriendPressed(sender: UIButton) {
     }
     
-    @IBAction func dropdownActionButtonPressed(sender: UIButton) {
+    @IBAction func dropdownActionButtonPressed(sender: UIButton)
+    {
+        if popupDrawerIsShowing {
+            UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.3, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                self.dropdownView.frame = CGRectMake(0, self.view.frame.size.height-self.dropdownActionButton.frame.size.height, self.dropdownView.frame.size.width, self.dropdownView.frame.size.height)
+            }, completion: nil)
+            
+            popupDrawerIsShowing = false
+        } else {
+            UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.3, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                self.dropdownView.frame = CGRectMake(0, self.view.frame.size.height-self.dropdownView.frame.size.height, self.dropdownView.frame.size.width, self.dropdownView.frame.size.height)
+                }, completion: nil)
+            popupDrawerIsShowing = true
+        }
     }
 }
